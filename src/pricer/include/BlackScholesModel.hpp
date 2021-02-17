400dea2e142ec611f098477d345527e550517f31
The Peridot <paul.dudnic@grenoble-inp.org>
2021-02-17 19:20:51 +0100

    BlackScholesModel::asset

--- src/pricer/include/BlackScholesModel.hpp
+++ src/pricer/include/BlackScholesModel.hpp
@@ -12,8 +12,8 @@ public:
     double r_;       /// taux d'intérêt
     double rho_;     /// paramètre de corrélation
     PnlVect *sigma_; /// vecteur de volatilités
-    PnlVect *divid;  /// vecteur des dividendes
-    PnlVect *spot_;  /// valeurs initiales des sous-jacents
+    PnlVect *divid_; /// vecteur des dividendes
+    PnlVect *spots_; /// valeurs initiales des sous-jacents
 
     /**
      * Génère une trajectoire du modèle et la stocke dans path
@@ -21,7 +21,25 @@ public:
      * @param[out] path contient une trajectoire du modèle.
      * C'est une matrice de taille (nbTimeSteps+1) x d
      * @param[in] T  maturité
-     * @param[in] nbTimeSteps nombre de dates de constatation
+     * @param[in] dates nombre de dates d'exercice
      */
     void asset(PnlMat *path, double T, int dates, PnlRng *rng);
+
+    /**
+     * Crée un modèle de Black-Scholes en remplissant ses attributs
+     * 
+     * @param[in] size est le nombre d'actifs du modèle
+     * @param[in] r est le taux d'intérêt
+     * @param[in] rho est le paramètre de corrélation
+     * @param[in] sigma est le vecteur des volatilités
+     * @param[in] divid est le vecteur des dividendes
+     * @param[in] spots est le vecteur des valeurs initiales des sous-jacents
+     */
+    BlackScholesModel(int size, double r, double rho, const PnlVect *sigma, const PnlVect *divid, const PnlVect *spots);
+
+    ~BlackScholesModel();
+
+private:
+    PnlMat *L_;
+    PnlVect *G_;
 };
