5b9f71e36dc7fb3c60455a379cc096598684d96b
The Peridot <paul.dudnic@grenoble-inp.org>
2021-02-17 17:08:31 +0100

    Options hpps

--- src/pricer/include/Option.hpp
+++ src/pricer/include/Option.hpp
@@ -7,9 +7,9 @@
 class Option
 {
 public:
-    double T_; /// maturité
+    double T_;  /// maturité
     int dates_; /// nombre de dates d'exercice
-    int size_; /// dimension du modèle, redondant avec BlackScholesModel::size_
+    int size_;  /// dimension du modèle, redondant avec BlackScholesModel::size_
     /**
      * Calcule la valeur du payoff sur la trajectoire
      *
@@ -19,6 +19,18 @@ public:
      * @return phi(trajectoire)
      */
     virtual double payoff(const PnlMat *path) = 0;
+
+    /**
+     * Crée une option en remplissant ses attributs
+     * 
+     * @param[in] T est la maturité de l'option
+     * @param[in] dates est le nombre de dates d'exercice de l'option
+     * @param[in] size est la dimension du modèle, il s'agit du nombre de sous-jacens de l'option
+     */
+    Option(double T, int dates, int size);
+
+    /**
+     * Detruit l'objet et libère la mémoire
+     */
+    virtual ~Option();
 };
-
-
